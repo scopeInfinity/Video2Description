a222247a1e29c826a8d1518f9faae2518dc95803
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-15 02:43:57 +0530

    [docker] pull scopeinfinity/video2description:deploy image

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker pull scopeinfinity/video2description:latest
-docker image build  --target v2d -t scopeinfinity/video2description --cache-from scopeinfinity/video2description:latest .
+docker pull scopeinfinity/video2description:deploy
+docker image build  --target v2d -t scopeinfinity/video2description:latest --cache-from scopeinfinity/video2description:latest .
 docker image build  --target v2d_deploy -t scopeinfinity/video2description:deploy --cache-from scopeinfinity/video2description:latest .
