a1922b72cc9db45202e953c090be1d8e7897f9f8
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-20 05:58:20 +0530

    [test] Added basic e2e test

--- docker_test.sh
+++ docker_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker container run scopeinfinity/video2description:latest conda run -n V2D /bin/bash -c 'cd /home/si/v2d/src/ && ./run_tests.sh'
 docker container run scopeinfinity/video2description:deploy conda run -n V2D /bin/bash -c 'cd /home/si/v2d/src/ && ./run_tests.sh'
+python -m unittest discover tests/
---
+++ tests/e2e/test_external.py
@@ -0,0 +1,59 @@
+import unittest
+import json
+import os
+
+from parameterized import parameterized
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+WEB_URL = "http://localhost:8080"
+ROOT_PATH = "src"
+CONFIG_FILE = "src/config.json"
+
+
+class TestExternal(unittest.TestCase):
+    """Test from as a end user."""
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('-headless')
+        self.driver = Firefox(firefox_options=options)
+
+    def tearDown(self):
+        self.driver.close()
+
+    def get_video_path(self, fname):
+        with open(CONFIG_FILE, "r") as fin:
+            dir_videos = json.load(fin)["tests"]["dir_videos"]
+            path = os.path.abspath(
+                os.path.join(ROOT_PATH, dir_videos, fname))
+            self.assertTrue(os.path.exists(path))
+            return path
+
+    @parameterized.expand([
+        ("12727.mp4", "two men are talking about a cooking show"),
+        ("12968.mp4", "a woman is talking about a makeup face"),
+    ])
+    def test_upload_and_verify(self, fname, caption):
+        """
+        Tests uploading a video and verify the response.
+        Note: The tests values are currently hard coded to a specific trained
+              model and it might fail for other models.
+        """
+        self.driver.get(WEB_URL)
+        video_path = self.get_video_path(fname)
+        text_vprocessing = "Video is being uploaded and processed"
+        self.driver.find_element_by_xpath("//input[@type='file']").send_keys(video_path)
+        self.assertNotIn(text_vprocessing,
+                         self.driver.find_element_by_id("notifications").text)
+        self.driver.find_element_by_xpath("//input[@value='Upload Video']").click()
+        self.assertIn(text_vprocessing,
+                      self.driver.find_element_by_id("notifications").text)
+        WebDriverWait(self.driver, 120).until(
+            EC.text_to_be_present_in_element(
+                (By.ID, "notifications"), "Request Completed")
+        )
+        self.assertIn(caption, self.driver.find_element_by_id("results").text.lower())
