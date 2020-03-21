c09f69759113fc803d3b8091f40eac214a295497
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-21 06:23:46 +0530

    Update docker_test.sh

--- docker_test.sh
+++ docker_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker container run scopeinfinity/video2description:latest conda run -n V2D /bin/bash -c 'cd /home/si/v2d/src/ && ./run_tests.sh'
 docker container run scopeinfinity/video2description:deploy conda run -n V2D /bin/bash -c 'cd /home/si/v2d/src/ && ./run_tests.sh'
+sleep 5m  # temporary hack
 python -m unittest discover tests/
