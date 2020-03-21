72fa524c1c9469f1ded1df89b6e4a4782c3800c3
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-21 22:06:24 +0530

    Fixed in docker_test.sh

--- docker_test.sh
+++ docker_test.sh
@@ -6,8 +6,8 @@ docker container run scopeinfinity/video2description:latest conda run -n V2D /bi
 docker container run scopeinfinity/video2description:deploy conda run -n V2D /bin/bash -c 'cd /home/si/v2d/src/ && ./run_tests.sh'
 
 bash docker_start.sh
-for x in `seq 10`;do
-    sleep "${TIMEOUT_WAIT_FOR_BACKEND}m";
+for x in `seq ${TIMEOUT_WAIT_FOR_BACKEND}`;do
+    sleep "1m";
     curl "http://localhost:8080/model_weights_status" 2> /dev/null | tee /dev/stderr | grep -q '\[SUCCESS\]' && break;
 done 2>&1 || { echo "Backend model_weights_status failed to come to success"; exit 1;}
 
