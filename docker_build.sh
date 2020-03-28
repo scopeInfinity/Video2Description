3c9b22113ea04021f68173f0ccbf83d9fbdb3df9
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-29 03:03:20 +0530

    Removing BUILDKIT_INLINE_CACHE

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-export DOCKER_BUILDKIT=1
 
 docker image build --target v2d \
   -t scopeinfinity/video2description:latest \
-  --build-arg BUILDKIT_INLINE_CACHE=1 \
   --cache-from scopeinfinity/video2description:latest \
   .
 
