9a613cf160ac64edf4d992d5855dd0adceda1051
Gagan Kumar <gagan1kumar.cs@gmail.com>
2020-03-29 03:17:11 +0530

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+export DOCKER_BUILDKIT=1
 
 docker image build --target v2d \
   -t scopeinfinity/video2description:latest \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
   --cache-from scopeinfinity/video2description:latest \
   .
 
 docker image build --target v2d_deploy \
   -t scopeinfinity/video2description:deploy \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
   --cache-from scopeinfinity/video2description:latest \
   --cache-from scopeinfinity/video2description:deploy \
   .
