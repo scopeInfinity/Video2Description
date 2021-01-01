93c4fcb8a4a5c0a449db22e7365d0bb5bfa18bc6
Gagan Kumar <gagan1kumar.cs@gmail.com>
2021-01-01 19:09:35 +0000

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-docker build \
-    --target ffmpeg_builder \
-    -t scopeinfinity/video2description:ffmpeg_builder \
-    -f "backend.Dockerfile" \
-    "."
+#docker build \
+#    --target ffmpeg_builder \
+#    -t scopeinfinity/video2description:ffmpeg_builder \
+#    -f "backend.Dockerfile" \
+#    "."
 
-docker build \
-    --target glove_builder \
-    -t scopeinfinity/video2description:glove_builder \
-    -f "backend.Dockerfile" \
-    "."
+#docker build \
+#    --target glove_builder \
+#    -t scopeinfinity/video2description:glove_builder \
+#    -f "backend.Dockerfile" \
+#    "."
 
-docker push scopeinfinity/video2description:ffmpeg_builder
-docker push scopeinfinity/video2description:glove_builder
+#docker push scopeinfinity/video2description:ffmpeg_builder
+#docker push scopeinfinity/video2description:glove_builder
 docker push scopeinfinity/video2description:frontend
 docker push scopeinfinity/video2description:deploy
